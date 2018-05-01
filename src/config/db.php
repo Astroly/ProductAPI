3ed3d0b29f14776c82e3cd8cb4cc9d2c22ab16f5
NitiponEXP <nitipon000@gmail.com>
2018-05-01 17:45:28 +0700

    b,j,

---
+++ src/config/db.php
@@ -0,0 +1,54 @@
+<?php
+    class db{
+        //Properties
+        // private $dbhoat = 'localhost';
+        // private $dbuser = 'root';
+        // private $dbpass = '123456';
+        // private $dbname = 'productservice';
+
+        //connect
+        public function connect(){
+            $dbhost = 'databases.000webhost.com';
+            $dbuser = 'id5591861_product';
+            $dbpass = '123456789';
+            $dbname = 'id5591861_productadmin';
+            // $mysql_connect_str = "$mysql:host=$this->dbhost;dbname=$this->dbname";
+            $dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
+            $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dbConnection->exec('SET NAMES utf8');
+            return $dbConnection;
+
+        }
+    }
+    function test() {
+        $dbhost = 'databases.000webhost.com';
+        $dbuser = 'id5591861_product';
+        $dbpass = '123456789';
+        $dbname = 'id5591861_productadmin';
+        try {
+            
+        // $dbpass = '';
+                $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+            
+                // set the PDO error mode to exception
+            
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+                echo "Connected successfully"; 
+            
+                }
+            
+            catch(PDOException $e)
+            
+                {
+            
+                echo "Connection failed: " . $e->getMessage();
+            
+                }
+            
+            
+    }
+    // test();
+    // echo '1234';
+
+?>
---
+++ src/routes/product.php
@@ -0,0 +1,220 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+// require '../vendor/autoload.php';
+$app = new \Slim\App;
+
+//Get All Products
+$app->get('/api/product', function(Request $request, Response $response){
+    // $sql = "SELECT productID,title,description,price,pic FROM product";
+    $sql = "SELECT * FROM product";
+
+    try{
+        //Get DB Object
+        $db = new db();
+        //Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $products = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($products);
+        // var_dump($products);
+
+
+    }catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+//GET Single Product
+$app->get('/api/product/{productID}',function(Request $request, Response $response){
+    $id = $request ->getAttribute('productID');
+    $sql = "SELECT * FROM product WHERE productID = $id";
+    
+    try {
+        $db = new db();
+        $db = $db->connect();
+
+         $stmt =$db->query($sql);
+
+        $product = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($product);
+    }catch(PDOException $e){
+        echo'{"error":{"text":}'.$e->getMessage().'}';
+    }
+});
+
+//Add Products
+
+$app->post('/api/product/add',function(Request $request, Response $response) {
+
+    $productID = $request->getParam('productID') ;
+
+    $title = $request->getParam('title') ;
+
+    $description = $request->getParam('description') ;
+
+    $price = $request->getParam('price') ;
+    $pic = $request->getParam('pic') ;
+
+
+
+    $sql ="INSERT INTO product (productID,title,description,price,pic) VALUES (:productID,:title,:description,:price,:pic)" ;
+
+    try {
+
+        //get DB object
+
+        $db = new db() ;
+
+        //connect
+
+        $db = $db->connect() ;
+
+
+
+        $stmt = $db->prepare($sql) ;
+
+
+
+        $stmt->bindParam(':productID',    $productID) ;
+
+        $stmt->bindParam(':title',     $title) ;
+
+        $stmt->bindParam(':description',    $description) ;
+
+        $stmt->bindParam(':price',    $price) ;
+        $stmt->bindParam(':pic',    $pic) ;
+
+
+
+        $stmt->execute() ;
+
+        echo '{"notice: {"text": "Product Add"}' ;
+
+
+
+    } catch(PDOExcaption $e) {
+
+            echo '{"error":{"text": '.$e->getMessage().'}' ;
+
+    }
+
+});
+
+
+
+//Update Products
+
+$app->put('/api/product/update/{id}',function(Request $request, Response $response) {
+
+    $id = $request->getAttribute('id');
+
+    $title = $request->getParam('title') ;
+
+    $description = $request->getParam('description') ;
+
+    $price = $request->getParam('price') ;
+    $pic = $request->getParam('pic') ;
+    
+
+
+
+    $sql = "UPDATE product SET
+
+            title = :title,
+
+            description = :description,
+
+            price = :price,
+            pic = :pic
+
+            WHERE productID=$id" ;
+
+    try{
+
+        //Get DB Object
+
+        $db = new db() ;
+
+        // Connect
+
+        $db = $db->connect() ;
+
+
+
+        $stmt = $db->prepare($sql) ;
+
+
+
+        // $stmt->bindParam(':productID',    $productID) ;
+
+        $stmt->bindParam(':title',     $title) ;
+
+        $stmt->bindParam(':description',    $description) ;
+
+        $stmt->bindParam(':price',    $price) ;
+        $stmt->bindParam(':pic',    $pic) ;
+
+
+
+        $stmt->execute() ;
+
+        echo '{"notice": {"text": "Product Update"}' ;
+
+
+
+    } catch(PODExution $e) {
+
+        echo '{"error": {"text": '.$e->getMessage().'}' ;
+
+    }
+
+});
+
+
+
+//Delete Product
+
+$app->delete('/api/product/delete/{id}', function(Request $request, Response $response){
+
+    $id = $request->getAttribute('id');
+
+    $sql = "DELETE FROM product WHERE productID=$id";
+
+    try{
+
+        //Get DB Object
+
+        $db = new db();
+
+        //connect
+
+        $db = $db->connect();
+
+
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->execute();
+
+        $db = null;
+
+        echo '{"notice": {"text": "Product Deleted"}';
+
+
+
+    }  catch(PDOException $e){
+
+        echo '{"error": {"text": '.$e->getMessage().'}';
+
+    }
+
+
+
+});
+
+?>
