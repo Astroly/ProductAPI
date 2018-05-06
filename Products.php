fb7c8e910043db8ed0ec78e13e356027996b2f1f
NitiponEXP <nitipon000@gmail.com>
2018-05-06 19:58:57 +0700

    try

--- Products.php
+++ Products.php
@@ -4,91 +4,91 @@ $app = new Slim\App;
 $app->get('/api/test', function ($request, $response) {
     return 'hello world';
 });
-$app->get('/api/products', function ($request, $response) {
-    header("Content-Type: application/json");
-    getProducts();
-});
-$app->get('/api/products/{id}', function ($request, $response, $args) {
+// $app->get('/api/products', function ($request, $response) {
+//     header("Content-Type: application/json");
+//     getProducts();
+// });
+// $app->get('/api/products/{id}', function ($request, $response, $args) {
 
 
-    $sql = "SELECT * FROM product where productID =  ('".$args['id']."')";
+//     $sql = "SELECT * FROM product where productID =  ('".$args['id']."')";
     
-    try {
-        $db = getConnection();
-        $stmt =$db->query($sql);
-        $product = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
-        echo json_encode($product);
-    }catch(PDOException $e){
-        echo'{"error":{"text":}'.$e->getMessage().'}';
-    }
-});
+//     try {
+//         $db = getConnection();
+//         $stmt =$db->query($sql);
+//         $product = $stmt->fetchAll(PDO::FETCH_OBJ);
+//         $db = null;
+//         echo json_encode($product);
+//     }catch(PDOException $e){
+//         echo'{"error":{"text":}'.$e->getMessage().'}';
+//     }
+// });
 
- $app->post('/api/products/add', function ($request, $response) {
+//  $app->post('/api/products/add', function ($request, $response) {
 
 
-    $db = getConnection();
-    $productID=$_POST["productID"];
-    $title = $_POST('title') ;
-    $picture = $_POST('picture') ;
-    $description = $_POST('description') ;
-    $price=$_POST["price"];
+//     $db = getConnection();
+//     $productID=$_POST["productID"];
+//     $title = $_POST('title') ;
+//     $picture = $_POST('picture') ;
+//     $description = $_POST('description') ;
+//     $price=$_POST["price"];
 
-    $query="INSERT INTO product SET productID='{$productID}', title='{$title}', price={$price}, picture={$picture}, price='{$price}'";
+//     $query="INSERT INTO product SET productID='{$productID}', title='{$title}', price={$price}, picture={$picture}, price='{$price}'";
     
     
     
- });
-$app->put('/api/products/update/{id}',function($request, $response, $args) {
-    header("Content-Type: application/json");
+//  });
+// $app->put('/api/products/update/{id}',function($request, $response, $args) {
+//     header("Content-Type: application/json");
     
-    $productID = $request->getParam('productID') ;
-    $title = $request->getParam('title') ;
-    $picture = $request->getParam('picture') ;
-    $description = $request->getParam('description') ;
-    $price = $request->getParam('price') ;
+//     $productID = $request->getParam('productID') ;
+//     $title = $request->getParam('title') ;
+//     $picture = $request->getParam('picture') ;
+//     $description = $request->getParam('description') ;
+//     $price = $request->getParam('price') ;
 
 
-    $sql = "UPDATE product SET
-            title = :title,
-            description = :description,
-            picture = :picture,
-            price = :price
+//     $sql = "UPDATE product SET
+//             title = :title,
+//             description = :description,
+//             picture = :picture,
+//             price = :price
 
-            WHERE productID=('".$args['id']."')" ;
-      $productID =  $args['id']   ;
-    try{
+//             WHERE productID=('".$args['id']."')" ;
+//       $productID =  $args['id']   ;
+//     try{
 
-        $db = getConnection();
-        $stmt =$db->query($sql);
-        $stmt->bindParam(':productID',    $productID) ;
-        $title = $request->getParam('title') ;
-        $picture = $request->getParam('picture') ;
-        $description = $request->getParam('description') ;
-        $price = $request->getParam('price') ;
-        $stmt->execute() ;
-        echo '{"notice": {"text": "Product Update"}' ;
+//         $db = getConnection();
+//         $stmt =$db->query($sql);
+//         $stmt->bindParam(':productID',    $productID) ;
+//         $title = $request->getParam('title') ;
+//         $picture = $request->getParam('picture') ;
+//         $description = $request->getParam('description') ;
+//         $price = $request->getParam('price') ;
+//         $stmt->execute() ;
+//         echo '{"notice": {"text": "Product Update"}' ;
 
-    } catch(PODExution $e) {
-        echo '{"error": {"text": '.$e->getMessage().'}' ;
+//     } catch(PODExution $e) {
+//         echo '{"error": {"text": '.$e->getMessage().'}' ;
 
-    }
-});
+//     }
+// });
 
-$app->delete('/api/products/delete/{id}', function($request, $response, $args) {
+// $app->delete('/api/products/delete/{id}', function($request, $response, $args) {
 
-    $sql = "DELETE FROM product WHERE productID = ('".$args['id']."')";
+//     $sql = "DELETE FROM product WHERE productID = ('".$args['id']."')";
     
-    try {
-        $db = getConnection();
-        $stmt =$db->query($sql);
-        $product = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
-        echo '{"notice": {"text": "Product Deleted"}';
-    }catch(PDOException $e){
-        echo json_encode($e->getMessage("Product Deleted"));
-    }
-});
+//     try {
+//         $db = getConnection();
+//         $stmt =$db->query($sql);
+//         $product = $stmt->fetchAll(PDO::FETCH_OBJ);
+//         $db = null;
+//         echo '{"notice": {"text": "Product Deleted"}';
+//     }catch(PDOException $e){
+//         echo json_encode($e->getMessage("Product Deleted"));
+//     }
+// });
 
 
 
@@ -97,19 +97,19 @@ $app->delete('/api/products/delete/{id}', function($request, $response, $args) {
 $app->run();
 
 
-function getProducts() {
-    $sql = "SELECT * FROM product";
-      try {
-        $db = getConnection();
-        $stmt = $db->query($sql);
-        $products = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
-        echo json_encode($products);
-      }
-      catch(PDOException $e) {
-        echo json_encode($e->getMessage());
-      }
-    }
+// function getProducts() {
+//     $sql = "SELECT * FROM product";
+//       try {
+//         $db = getConnection();
+//         $stmt = $db->query($sql);
+//         $products = $stmt->fetchAll(PDO::FETCH_OBJ);
+//         $db = null;
+//         echo json_encode($products);
+//       }
+//       catch(PDOException $e) {
+//         echo json_encode($e->getMessage());
+//       }
+//     }
 
     
 function getConnection() {
