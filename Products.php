e943859874ab26815cc8f8329c06c9d3b02ba686
poramet003 <s5830213003@phuket.psu.ac.th>
2018-05-06 22:58:09 +0700

    add description

--- Products.php
+++ Products.php
@@ -37,10 +37,8 @@ $app->get('/api/products/{id}', function ($request, $response, $args) {
     $description = $_POST('description') ;
     $price=$_POST["price"];
 
-    // $query="INSERT INTO product SET productID='{$productID}', title='{$title}',
-    //  price={$price}, picture={$picture}, price='{$price}'";
-   $query= "INSERT INTO product (title , description , picture, price)
-    VALUES ($title,  $description, $picture,  $price)"
+    $query="INSERT INTO product SET productID='{$productID}', title='{$title}', picture={$picture},description={$description}, price='{$price}'";
+   //$query= "INSERT INTO product (title , description , picture, price) VALUES ('{$title}',  $description, $picture,  $price)"
     
     
  });
