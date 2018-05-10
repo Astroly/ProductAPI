af73660dadbb6c7d75574cea968e68aed4dbc22f
NitiponEXP <nitipon000@gmail.com>
2018-05-10 16:29:11 +0700

    sdgvad

--- Products.php
+++ Products.php
@@ -40,12 +40,12 @@ $app->get('/api/products/{id}', function ($request, $response, $args) {
       $picture = $request->getParam('picture') ;
       $description = $request->getParam('description') ;
       $price = $request->getParam('price') ;
-      $weight = $request->getParam('weight'+'('+'kg'+')') ;
+      $weight = $request->getParam('weight(kg)') ;
      
 try {
     $db = getConnection();
     
-    $sql="INSERT INTO product(productID,title,picture,description,price,weight(kg))
+    $sql="INSERT INTO product(productID,title,picture,description,price,'weight(kg)')
     VALUES ('" .$productID."','" .$title."','" .$picture."' ,'" .$description."','" .$price."','" .$weight."')";
     $stmt = $db->query($sql);
     $db = null;
@@ -75,7 +75,7 @@ $sql="UPDATE product SET
     picture=('".$picture."'),
     description=('".$description."'),
     price=('".$price."'),
-    weight(kg)=('".$weight."')
+    'weight(kg)'=('".$weight."')
     
      WHERE productID= ('".$args['id']."') " ;  
 $stmt = $db->query($sql);
